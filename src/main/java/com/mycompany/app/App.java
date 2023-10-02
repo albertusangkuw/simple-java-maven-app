51e5427a511c9003004448f7b006155fb3033b5f
Albertus A <angkuwjr@yahoo.com>
2023-10-02 08:20:03 +0700

    Test update source code aplikasi

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,11 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
+        System.out.println("--- Submision Proyek Membangun CI/CD Pipeline dengan Jenkins (Dicoding)  ----");
+        System.out.println("... Java App ....");
+        System.out.println("Albertus Septian Angkuw");
         System.out.println(MESSAGE);
+        System.out.println("[App Exited]");
     }
 
     public String getMessage() {
